ea55b5e88df08afaea2882b5db19752cbde87bd1
Mohd Aquib <akibkhan9984@gmail.com>
2025-03-18 15:08:30 +0530

    project page updated with project card comp

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 import React from "react";
 import { useSidebar } from "@/context/SidebarContext";
-import { BorderBeam } from "@/components/magicui/border-beam";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import ProjectCard from "@/components/project/ProjectCard";
 
 // Sample Projects Array
 const projects = [
@@ -152,57 +149,7 @@ const ProjectPage = () => {
       {/* Project Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
         {projects.map((project) => (
-          <div key={project.title} className="relative p-[2px] rounded-lg">
-            <BorderBeam duration={8} size={200} />
-            <div className="bg-[#122738] rounded-lg shadow-md shadow-[#000]/[0.3] overflow-hidden h-full">
-              {/* Image Section */}
-              <div className="w-full h-44 overflow-hidden">
-                <Image
-                  src={project.image}
-                  alt={project.title}
-                  layout="responsive"
-                  width={220}
-                  height={140}
-                  className="rounded-t-lg object-cover"
-                />
-              </div>
-
-              {/* Content Section */}
-              <div className="py-2 px-3 text-slate-200">
-                <h2 className="text-lg font-bold">{project.title}</h2>
-                <p className="text-xs leading-5 text-gray-300 my-1">
-                  {project.description}
-                </p>
-
-                {/* Technology Tags */}
-                <div className="flex flex-wrap gap-2 my-2">
-                  {project.technologies.map((tech, index) => (
-                    <Button key={index} className="bg-vscodeHeader hover:bg-vscodeHeader">
-                      {tech}
-                    </Button>
-                  ))}
-                </div>
-
-                {/* Links */}
-                <div className="mt-3 flex justify-between items-center">
-                  <Link
-                    href={project.liveLink}
-                    target="_blank"
-                    className="text-xs text-vscodeAccent capitalize font-semibold"
-                  >
-                    View Project
-                  </Link>
-                  <Link
-                    href={project.githubLink}
-                    target="_blank"
-                    className="text-xs text-vscodeAccent capitalize font-semibold"
-                  >
-                    GitHub
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
+          <ProjectCard key={project.title} project={project}/>
         ))}
       </div>
     </main>
