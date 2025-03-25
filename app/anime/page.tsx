1faea5bb2d540260a6a6ab6e8c8d046acf5c322f
Mohd Aquib <akibkhan9984@gmail.com>
2025-03-25 23:52:28 +0530

    bold

--- app/anime/page.tsx
+++ app/anime/page.tsx
@@ -115,7 +115,7 @@ const AnimePage = () => {
   return (
     <main className={`${isSidebarOpen ? "lg:ml-80 ml-12" : "ml-12"}  mt-20 pb-20`}>
       <div className="mb-10">
-        <h1 className="md:text-4xl text-3xl font-boldfont-bold text-white mb-2 leading-normal">Living Between Reality and Anime</h1>
+        <h1 className="md:text-4xl text-3xl font-bold text-white mb-2 leading-normal">Living Between Reality and Anime</h1>
         <p className="text-sm">Whether its action, mystery, or heartwarming moments, anime has a way of capturing emotions like nothing else. Sometimes I watch, sometimes I rewatch—but I’m always part of this universe.</p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
