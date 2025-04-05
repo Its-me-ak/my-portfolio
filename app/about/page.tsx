2467737caf51a9c56723263bb09ca4f8f042ef95
Mohd Aquib <akibkhan9984@gmail.com>
2025-04-05 15:47:06 +0530

    github contribution added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,16 +4,32 @@ import Experience from "@/components/about/Experience";
 import Me from "@/components/about/Me";
 import { useSidebar } from "@/context/SidebarContext";
 import React from "react";
+import GitHubCalendar from 'react-github-calendar';
 
 const AboutPage = () => {
   const { isSidebarOpen } = useSidebar();
+
   return (
-    <main className={`${isSidebarOpen ? "lg:ml-80 ml-12" : "ml-12"}  mt-20 pb-20`}>
+    <main className={`${isSidebarOpen ? "lg:ml-80 ml-12" : "ml-12"} mt-20 pb-20`}>
       <Me />
       <div className="flex lg:flex-row flex-col justify-between pt-16 gap-10">
         <Experience />
         <Education />
       </div>
+
+      <div className="lg:w-full w-80 sm:w-[560px] md:w-[700px] pt-16">
+        <h2 className="text-xl font-semibold mb-4 text-white">GitHub Contributions</h2>
+
+        <div className="overflow-x-auto rounded-lg p-4 bg-[#122738]">
+          <GitHubCalendar
+            username="its-me-ak"
+            blockSize={17}
+            blockMargin={5}
+            fontSize={14}
+          />
+        </div>
+      </div>
+
     </main>
   );
 };
--- components/about/Me.tsx
+++ components/about/Me.tsx
@@ -11,51 +11,53 @@ const Me = () => {
   const { isSidebarOpen } = useSidebar();
 
   return (
-    <div className="flex lg:flex-row flex-col gap-2 justify-between">
-      <div
-        className={`${isSidebarOpen ? "max-w-xl" : "max-w-4xl"
-          }  text-justify space-y-3`}
-      >
-        <h3 className="text-md font-semibold text-vscodeAccent">
-          <TextAnimate animate="slideUp" by="word">
-            Hello, Welcome
-          </TextAnimate>
-        </h3>
-        <h1 className="md:text-4xl text-3xl font-bold">
-          <TextAnimate animate="slideLeft" by="word">
-            I&apos;m Mohd Aquib
-          </TextAnimate>
-        </h1>
-        <div className="pb-4">
-          <TypingAnimation className="text-md" duration={30}>
-            A passionate frontend developer with a flair for crafting immersive
-            and visually appealing web experiences. Fluent in the languages of
-            the web, I&apos;m well-versed in HTML, CSS, SASS, Bootstrap, JavaScript,
-            and WordPress. I have a strong command of modern frontend frameworks
-            such as React, and I&apos;m always eager to explore new tools and
-            technologies that push the boundaries of what&apos;s possible on the web.
-          </TypingAnimation>
+    <>
+      <div className="flex lg:flex-row flex-col gap-2 justify-between">
+        <div
+          className={`${isSidebarOpen ? "max-w-xl" : "max-w-4xl"
+            }  text-justify space-y-3`}
+        >
+          <h3 className="text-md font-semibold text-vscodeAccent">
+            <TextAnimate animate="slideUp" by="word">
+              Hello, Welcome
+            </TextAnimate>
+          </h3>
+          <h1 className="md:text-4xl text-3xl font-bold">
+            <TextAnimate animate="slideLeft" by="word">
+              I&apos;m Mohd Aquib
+            </TextAnimate>
+          </h1>
+          <div className="pb-4">
+            <TypingAnimation className="text-md" duration={30}>
+              A passionate frontend developer with a flair for crafting immersive
+              and visually appealing web experiences. Fluent in the languages of
+              the web, I&apos;m well-versed in HTML, CSS, SASS, Bootstrap, JavaScript,
+              and WordPress. I have a strong command of modern frontend frameworks
+              such as React, and I&apos;m always eager to explore new tools and
+              technologies that push the boundaries of what&apos;s possible on the web.
+            </TypingAnimation>
+          </div>
+          <div className="flex gap-5">
+            <Link href={"/contact"}>
+              <ShimmerButton>Contact Me</ShimmerButton>
+            </Link>
+            <Link href="assests/Mohd_Aquib_Frontend_Developer_CV.pdf" target="_blank" rel="noopener noreferrer" download>
+              <ShimmerButton>Download CV</ShimmerButton>
+            </Link>
+          </div>
         </div>
-        <div className="flex gap-5">
-          <Link href={"/contact"}>
-            <ShimmerButton>Contact Me</ShimmerButton>
-          </Link>
-          <Link href="assests/Mohd_Aquib_Frontend_Developer_CV.pdf" target="_blank" rel="noopener noreferrer" download>
-            <ShimmerButton>Download CV</ShimmerButton>
-          </Link>
+        <div className="relative rounded-xl lg:h-[450px] lg:w-[450px] lg:mt-0 mt-6 ">
+          <Image
+            src={"/assests/about.jpg"}
+            alt="About Image"
+            height={450}
+            width={500}
+            className="rounded-xl"
+          />
+          <BorderBeam duration={8} size={200} />
         </div>
       </div>
-      <div className="relative rounded-xl lg:h-[450px] lg:w-[450px] lg:mt-0 mt-6 ">
-        <Image
-          src={"/assests/about.jpg"}
-          alt="About Image"
-          height={450}
-          width={500}
-          className="rounded-xl"
-        />
-        <BorderBeam duration={8} size={200} />
-      </div>
-    </div>
+    </>
   );
 };
 
